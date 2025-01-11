35f45e99f5e776ecbff2cd2b8b9aeed6ef76b1cf
jbenquerenca <jbenquerenca9@gmail.com>
2025-01-11 01:24:34 +0000

    script to plot metrics during training

---
+++ tools/plot.py
@@ -0,0 +1,35 @@
+# read train from log and plots metrics
+import os, matplotlib.pyplot as plt, numpy as np, sys
+if len(sys.argv) == 1: print("You forgot the log path!"); exit()
+with open(sys.argv[1]) as f: 
+    results = {m:list() for m in ["models", "reasonable", "reasonable_small", "heavy", "all"]}
+    lines = f.read().splitlines()
+    for i, l in enumerate(lines):
+        if "Reasonable" in l:
+            sl = l.split()
+            results["models"].append(lines[i+1].split()[8])
+            results["reasonable"].append(float(sl[5].split("%")[0])/100)
+            results["reasonable_small"].append(float(sl[7].split("%")[0])/100)
+            results["heavy"].append(float(sl[9].split("%")[0])/100)
+            results["all"].append(float(sl[11].split("%")[0])/100)
+print(results)
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 8))
+ax1.set_title('reasonable')
+ax1.set_xlabel('X axis')
+ax1.set_ylabel('Y axis')
+ax1.plot(results["models"], results["reasonable"], "-o")
+ax2.set_title('reasonable_small')
+ax2.set_xlabel('X axis')
+ax2.set_ylabel('Y axis')
+ax2.plot(results["models"], results["reasonable_small"], "-o")
+ax3.set_title('heavy')
+ax3.set_xlabel('X axis')
+ax3.set_ylabel('Y axis')
+ax3.plot(results["models"], results["heavy"], "-o")
+ax4.set_title('all')
+ax4.set_xlabel('X axis')
+ax4.set_ylabel('Y axis')
+ax4.plot(results["models"], results["all"], "-o")
+plt.tight_layout()
+plt.savefig('plots.png', bbox_inches='tight')
+plt.close()
